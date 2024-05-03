e54d402aea35bd85ed45fac7d4a7c463f532efaa
Guilherme Santana <guiguilsantana@gmail.com>
2024-05-03 13:41:07 -0300

    Adicionando o texto, os  botões e suas estilizações

--- Calculadora/calculadora.py
+++ Calculadora/calculadora.py
@@ -1,6 +1,28 @@
 import flet as ft
 from flet import colors
 
+botoes = [
+    {'operador': 'AC', 'fonte': colors.BLACK, 'fundo': colors.BLUE_GREY_100},
+    {'operador': '+-', 'fonte': colors.BLACK, 'fundo': colors.BLUE_GREY_100},
+    {'operador': '%', 'fonte': colors.BLACK, 'fundo': colors.BLUE_GREY_100},
+    {'operador': '/', 'fonte': colors.WHITE, 'fundo': colors.ORANGE},
+    {'operador': '7', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '8', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '9', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '*', 'fonte': colors.WHITE, 'fundo': colors.ORANGE},
+    {'operador': '4', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '5', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '6', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '-', 'fonte': colors.WHITE, 'fundo': colors.ORANGE},
+    {'operador': '1', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '2', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '3', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '+', 'fonte': colors.WHITE, 'fundo': colors.ORANGE},
+    {'operador': '0', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '.', 'fonte': colors.WHITE, 'fundo': colors.WHITE24},
+    {'operador': '=', 'fonte': colors.WHITE, 'fundo': colors.ORANGE},
+]
+
 def main(page: ft.page):
     page.bgcolor = '#fff'
     page.window_resizable = False
@@ -17,6 +39,22 @@ def main(page: ft.page):
         alignment = 'end'
     )
 
-    page.add(display)
+    btn = [ft.Container(
+        content = ft.Text(value=btn['operador'], color=btn['fonte']),
+        width = 70,
+        height = 70,
+        bgcolor = btn['fundo'],
+        border_radius = 100,
+        alignment = ft.alignment.center,
+    )   for btn in botoes]
+
+    keyboard = ft.Row(
+        width = 420,
+        wrap = True,
+        controls = btn,
+        alignment =  'end'
+    )
+
+    page.add(display,keyboard)
 
 ft.app(target = main)
