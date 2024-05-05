1a37970cf4715e10d16ed19f3e4343b3a518b36c
Guilherme Santana <guiguilsantana@gmail.com>
2024-05-05 20:35:22 -0300

    Adicionando colisão com o própio corpo da cobra e das paredes

--- Jogo_cobrinha/snake.py
+++ Jogo_cobrinha/snake.py
@@ -1,11 +1,24 @@
 import pygame
 from pygame.locals import *
+import random
 
 WINDOW_SIZE = (600, 600)
 PIXEL_SIZE = 10
 
 def colision(pos1, pos2):
     return pos1 == pos2
+
+def off_limits(pos):
+    if 0 <= pos[0] < WINDOW_SIZE[0] and 0<= pos[1] < WINDOW_SIZE[1]:
+        return False
+    else:
+        return True
+    
+def random_on_grid():
+    x = random.randint(0, WINDOW_SIZE[0])
+    y = random.randint(0, WINDOW_SIZE[1])
+    return x // PIXEL_SIZE * PIXEL_SIZE, y // PIXEL_SIZE * PIXEL_SIZE
+    
 
 
 pygame.init()
@@ -19,9 +32,10 @@ snake_direction = K_LEFT
 
 apple_surface = pygame.Surface((10, 10))
 apple_surface.fill((255, 0, 0))
+apple_pos = random_on_grid()
 
 while True:
-    pygame.time.Clock().tick(60)
+    pygame.time.Clock().tick(30)
     screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -41,13 +55,21 @@ while True:
     elif snake_direction == K_RIGHT:
         snake_pos[0] = (snake_pos[0][0] + PIXEL_SIZE, snake_pos[0][1])
 
+ 
+    screen.blit(apple_surface, apple_pos)
+
     for pos in snake_pos:
         screen.blit(snake_surface, pos)
+
 
     for i in range(len(snake_pos) - 1, 0, -1):
         if colision(snake_pos[0], snake_pos[i]):
             pygame.quit()
             quit()
         snake_pos[i] = snake_pos[i-1]
+
+        if off_limits(snake_pos[0]):
+            pygame.quit()
+            quit()
 
     pygame.display.update()
